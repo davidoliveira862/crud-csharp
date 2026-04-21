94289b3f3ce3c3b470f8d1aa840523890d5d44d5
davidoliveira862 <soaresdavid862@gmail.com>
2026-04-21 07:16:49 -0300

    alterado variavel DAteTime

--- store/Models/Store.cs
+++ store/Models/Store.cs
@@ -8,11 +8,13 @@ namespace store.Models
     public class Store
     {
         public int Id { get; set; }
-        public DateTime Entrada { get; set; }
+        public DateTime Entrada { get; set; } = DateTime.UtcNow;
         public string Produto { get; set; }
         public double Preco { get; set; }
         public string Tamanho { get; set; }
         public string Marca { get; set; }
         
     }
+
+    
 }
