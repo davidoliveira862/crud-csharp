37abf3c6ebc9b5b89c92f3fe81de66ac52fb819b
davidoliveira862 <soaresdavid862@gmail.com>
2026-04-21 08:51:02 -0300

    atualização para mostrar hora local

--- store/Data/AppDBContext.cs
+++ store/Data/AppDBContext.cs
@@ -7,10 +7,26 @@ using store.Models;
 
 namespace store.Data
 {
-    public class AppDBContext : DbContext
-    {
-        public AppDBContext(DbContextOptions options) : base(options){}
+   public class AppDBContext : DbContext
+{
+    public AppDBContext(DbContextOptions options) : base(options){}
 
-        public DbSet<Store> StoreImports {get; set;} 
+    public DbSet<Store> StoreImports { get; set; }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries<Store>();
+
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added || entry.State == EntityState.Modified)
+            {
+                entry.Entity.Entrada = DateTime.Now;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
     }
+}
+
 }
