bbbd06c8bdf11144c85b53c954b51ca6429a7e86
davidoliveira862 <soaresdavid862@gmail.com>
2026-04-21 12:05:18 -0300

    hora utcNow

--- store/Models/Store.cs
+++ store/Models/Store.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,7 +7,7 @@ namespace store.Models
     public class Store
     {
         public int Id { get; set; }
-        public DateTime Entrada { get; set; }
+        public DateTime Entrada { get; set; } = DateTime.UtcNow;
         public string Produto { get; set; }
         public double Preco { get; set; }
         public string Tamanho { get; set; }
